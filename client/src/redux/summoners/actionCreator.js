8849b9c116ffd640d953257fc77d571a2690e924
YUJIN <21900630@handong.edu>
2022-07-20 14:45:44 +0900

    api key modified

--- client/src/redux/summoners/actionCreator.js
+++ client/src/redux/summoners/actionCreator.js
@@ -12,7 +12,7 @@ const getSummoner = () => {
         try {
             await dispatch(summonerBegin());
             const summoner = await axios.get(
-                process.env.REACT_APP_RESTAPI_HOST+"장나나냐?api_key=RGAPI-914fd54f-3b53-45da-b4be-b8e0a2e1a2a3"
+                "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/장나나냐?api_key=RGAPI-914fd54f-3b53-45da-b4be-b8e0a2e1a2a3"
             );
             console.log(summoner);
             await dispatch(summonerSuccess(summoner.data));
